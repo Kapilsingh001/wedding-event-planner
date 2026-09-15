f94dde813cc6443cbd2e497d5d2d2b620a3da11a
Kapilsingh001 <ks1878154@gmail.com>
2026-09-15 20:57:46 +0530

    Add routing, pages, navbar and JSON database setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-function App() {
-  return <h1>Wedding and Event Planning Management Platform</h1>
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Login from './features/auth/Login.jsx'
+import Register from './features/auth/Register.jsx'
+import Vendors from './features/vendors/Vendors.jsx'
+import Events from './features/planning/Events.jsx'
+
+function App(){
+  return(
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+       <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/vendors" element={<Vendors />} />
+        <Route path="/events" element={<Events />} />
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
+  )
 }
 
 export default App
