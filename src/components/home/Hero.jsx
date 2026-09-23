1a9d77f9dacfea2321197f0a0dbe671ff0e6d56c
Kapilsingh001 <ks1878154@gmail.com>
2026-09-23 11:05:22 +0530

    removing dummy heading

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -44,7 +44,7 @@ function Hero() {
         </div>
 
         <div className="hero-visual">
-          <p className="hero-sample">Example plan &middot; sample data</p>
+          {/* <p className="hero-sample">Example plan &middot; sample data</p> */}
           <img
             className="hero-photo"
             src="/images/hero.webp"
